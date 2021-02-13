355cc4ecf2b51de78e91a918f95ba3884792bdcc
Michael R. Nelli <mrn73@snow.cs.rutgers.edu>
2021-02-13 03:36:53 -0500

    cute

--- project1/syscall.c
+++ project1/syscall.c
@@ -1,28 +1,33 @@
 /* syscall.c
  *
  * Group Members Names and NetIDs:
- *   1.
- *   2.
+ *   1. Michael Nelli (mrn73)
+ *   2. Christopher Naporlee (cmn134)
  *
- * ILab Machine Tested on:
+ * ILab Machine Tested on: snow.cs.rutgers.edu
  *
  */
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>                                                                                
+#include <sys/time.h>
 #include <sys/syscall.h>
 
 double avg_time = 0;
 
 int main(int argc, char *argv[]) {
+	struct timeval start, end;
+	int i;
 
-    /* Implement Code Here */
+	for (i = 0; i < 3000; i++) {
+		gettimeofday(&start, NULL);
+		syscall(SYS_getpid);
+		gettimeofday(&end, NULL);
+		avg_time += (double)(end.tv_usec - start.tv_usec) + (double)(end.tv_sec - start.tv_sec) * 1e6;
+	}
 
-    // Remember to place your final calculated average time
-    // per system call into the avg_time variable
+	avg_time /= 3000;
 
-    printf("Average time per system call is %f microseconds\n", avg_time);
+	printf("Average time per system call is %f microseconds\n", avg_time);
 
-    return 0;
+	return 0;
 }
